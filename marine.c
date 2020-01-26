b966ab778126b0937945a709101ec309a35b4894
johnjeeva <johnsjeeva@gmail.com>
2020-01-26 02:50:01 +0000

    commit 4

--- marine.c
+++ marine.c
@@ -3,6 +3,11 @@
 
 #include"marine.h"
 #include"sensor.h"
+#include"network.h"
+#include"decision.h"
+#include"display.h"
+#include"cloud.h"
+#include"controls.h"
 int main(int argc, char argv[])
 {
 	int result = 0;
@@ -13,6 +18,16 @@ int main(int argc, char argv[])
 		printf("sensor value fetching failed from main function\n");
 	}
 	printf("sensor value fetched  in main function:%d\n",sensor.temperature);
+	printf("sensor value fetched  in main function:%d\n",sensor.humidity);
+	printf("sensor value fetched  in main function:%d\n",sensor.atmosphere_pressure);
+	printf("sensor value fetched  in main function:%d\n",sensor.water_level);
+	printf("sensor value fetched  in main function:%d\n",sensor.storage_box);
+	printf("sensor value fetched  in main function:%d\n",sensor.wind_speed);
+	printf("sensor value fetched  in main function:%d\n",sensor.wind_direction);
+	printf("sensor value fetched  in main function:%d\n",sensor.engine_heat);
+	printf("sensor value fetched  in main function:%d\n",sensor.engine_room_fire);
+	printf("sensor value fetched  in main function:%d\n",sensor.battery_amp);
+	printf("sensor value fetched  in main function:%d\n",sensor.engine_status);
 	return 0;
 }
 
