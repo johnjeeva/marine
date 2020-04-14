0b752e0013e23a8c7f9d71ebaee225ca3a0a2d02
sibysagar <sagarsiby@gmail.com>
2020-04-14 16:16:15 +0530

    Celsius

--- thermistor.c
+++ thermistor.c
@@ -1,7 +1,7 @@
 float Thermistor_function(int Vo) {
- logRt = log(10000.0*((1024.0/Vo-1))); 
- T = (1.0 / (A + B*logRt + C*logRt*logRt*logRt));  // We get the temperature value in Kelvin from this Stein-Hart equation
- Tc = T - 273.15;                     // Convert Kelvin to Celcius
- Tf = (Tc * 1.8) + 32.0;              // Convert Kelvin to Fahrenheit
- return T;
+Â logRt = log(10000.0*((1024.0/Vo-1)));Â 
+Â T = (1.0 / (A + B*logRt + C*logRt*logRt*logRt));Â  // We get the temperature value in Kelvin from this Stein-Hart equation
+Â Tc = T - 273.15;Â  Â  Â  Â  Â  Â  Â  Â  Â  Â  Â // Convert Kelvin to Celcius
+Â ////Tf = (Tc * 1.8) + 32.0;Â  Â  Â  Â  Â  Â  Â  // Convert Kelvin to Fahrenheit
+Â return Tc;
 }
