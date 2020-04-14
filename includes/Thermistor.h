1a95ce681d5c74471a931d0b953ce1d58461b5a6
sibysagar <sagarsiby@gmail.com>
2020-04-14 16:11:12 +0530

    Thermistor
    .h file of thermistor

---
+++ includes/Thermistor.h
@@ -0,0 +1,6 @@
+////////////////////Variables for Thermistor///////////
+float A = 1.009249522e-03, B = 2.378405444e-04, C = 2.019202697e-07;
+float T,logRt,Tf,Tc;
+
+//////////Function declaration for Thermistor/////////////
+float Thermistor_function(int Vo);
